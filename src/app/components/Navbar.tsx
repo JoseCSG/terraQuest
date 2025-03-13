be9370b935a0a71a703e98f7bd3c511da591a4c1
José Carlos Sánchez <a01174050@tec.mx>
2025-03-12 20:01:49 -0600

    delete optimized image

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -4,7 +4,13 @@ export default function Navbar({ score }: { score: number }) {
   return (
     <nav className="items-center justify-between bg-purple-900 px-4 py-6 rounded-bl-xl rounded-br-xl flex flex-row w-full">
       <div className="flex flex-row items-center gap-2">
-        <Image src={"/logo.png"} width={50} height={50} alt="TerraQuest Logo" />
+        <Image
+          src={"/logo.png"}
+          width={50}
+          height={50}
+          alt="TerraQuest Logo"
+          unoptimized={true}
+        />
         <p className="font-bold text-white hidden sm:block text-2xl">
           TerraQuest
         </p>
